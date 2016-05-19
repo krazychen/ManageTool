f2613ba6494898fdb905dac43a83fec65ddfe7df
Krazy <37485345@qq.com>
2016-05-19 15:43:34 +0800

    1. re-try new chat screen 2. dynamic add user in list 3. user login

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,17 +1,32 @@
 $(function() {
+    var username;
+    var selectedUserName;
     var socket = io.connect('http://localhost:3000');
-    socket.on('chat message', function(msg){
-        appendMessage(msg);
-    });
 
+    //log in message
+    function loginMessage(){
+        //get current usernamae
+        username=$("#username").text();
+        $('.inputMessage').focus();
+        // Tell the server your username
+        socket.emit('add user', username);
+    }
 
-    function appendMessage(msg){
-        var $messageBodyDiv = $('<span class="messageBody">')
-            .text(msg);
-        var $messageDiv = $('<li class="message"/>')
-            .append( $messageBodyDiv);
-        $('.messages').append($messageDiv);
-        $('.messages')[0].scrollTop =$('.messages')[0].scrollHeight;
+    function appendMessage(data){
+        var $whoDiv;
+        if(username==data.username){
+            $whoDiv = $('<div class="who" style="color:green">').text(data.username);
+        }else{
+            $whoDiv = $('<div class="who" style="color:blue">').text(data.username);
+        }
+        var $whenDiv=$('<div class="when" >').text((new Date()).toLocaleString());
+        var $headerDiv=$('<div class="messageHeader messageClearfix">').append($whoDiv).append($whenDiv);
+        var $messageDiv =$('<div class="msg" >').text(data.msg);
+
+        var $liItem =$('<li>').append($headerDiv).append($messageDiv);
+
+        $('.messagesul').append($liItem);
+        $('.messagesul')[0].scrollTop =$('.messagesul')[0].scrollHeight;
     }
 
     function sendMessage(event){
@@ -27,5 +42,47 @@ $(function() {
         }
     }
 
-    $("#chatSend").keydown(function(event){sendMessage(event)});
+    function refreshUserList(userList){
+        $("#userList").empty();
+        for(var i=0;i<userList.length;i++){
+            var username=userList[i];
+            var faWexin=$('<i class="fa fa-weixin">');
+            var pItem=$('<p>').text("  "+username).prepend(faWexin);
+            //var aItem=$('<a href="#" class="selectUser">').append(pItem);
+            var liItem=$('<li id="'+username.trim()+'"'+' class="selectUser">').append(pItem);
+
+            //add to userlist
+            $("#userList").append(liItem);
+        }
+        // if selected user is not null, still selected user
+        if(selectedUserName){
+            $("#"+selectedUserName).css("background", "#bbbbbb");
+        }
+        registerUserSelectEvent();
+    }
+
+    function registerUserSelectEvent() {
+
+        $(".selectUser").click(function (e) {
+            $('.selectUser').css("background", "#f3f3f3");
+            $(this).css("background", "#bbbbbb");
+
+            //current selected user name
+            selectedUserName=$(this).text().trim();
+        });
+    }
+
+    $(window).keydown(function(event){sendMessage(event)});
+
+    socket.on('user joined',function(msg){
+        refreshUserList(msg.userList);
+    });
+
+
+    socket.on('chat message', function(msg){
+        appendMessage(msg);
+    });
+
+    //tell server i have login
+    loginMessage();
 });
--- routes/chat.js
+++ routes/chat.js
@@ -2,11 +2,40 @@ module.exports=function(io){
     var app = require('express');
     var router = app.Router();
 
+    var userList=new Array();
+
     /** message connection **/
     io.on('connection', function(socket){
-        socket.on('chat message', function (data) {
-            console.log("12321:"+data);
-            io.emit('chat message', data);
+
+        var addedUser=false;
+
+        socket.on('add user',function(username){
+            if(addedUser) return;
+
+            //to avoid duplicate username add in list;
+            if(userList.indexOf(username)!=-1){
+                return;
+            }
+            userList.push(username);
+            addedUser=true;
+            socket.username = username;
+
+            //login inform
+            //socket.emit('login',{userList:userList});
+            console.log(userList);
+            // echo globally (all clients) that a person has connected
+            io.emit('user joined', {
+                username: socket.username,
+                userList:userList
+            });
+        })
+
+
+        socket.on('chat message', function (msg) {
+            io.emit('chat message', {
+             username:socket.username,
+             msg:msg
+            });
         });
     });
 
